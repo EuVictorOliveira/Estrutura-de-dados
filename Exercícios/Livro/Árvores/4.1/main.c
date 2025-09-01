eb96405bd45789911e1231e911ad36df1ca90e0c
Victor Oliveira <victoroliver249@gmail.com>
2025-08-31 21:57:20 -0300

    Implementa exercício 4.1

---
+++ Exercícios/Livro/Árvores/4.1/main.c
@@ -0,0 +1,42 @@
+/*
+    Programa principal para testar a implementação da Árvore Binária
+    Autor: João Victor Oliveira
+    Data: 30/08/2025
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Arvore.h"
+
+int main() {
+    /*     Cria uma árvore simples:
+                  10
+                /    \
+              12       20
+             / \     /  \
+            4   7   15   25 
+    */
+
+    Arvore* raiz = cria_arvore(
+        10,
+        cria_arvore(
+            12,
+            cria_arvore(4, NULL, NULL),
+            cria_arvore(7, NULL, NULL)
+        ),
+        cria_arvore(
+            20,
+            cria_arvore(15, NULL, NULL),
+            cria_arvore(25, NULL, NULL)
+        )
+    );
+
+    // Exibe árvore em pré-ordem
+    printf("Exibindo arvore (pre-ordem): ");
+    exibir_arvore(raiz);
+    printf("\n");
+
+    printf("Quantidade de pares: %d\n", pares(raiz));
+
+    return 0;
+}
